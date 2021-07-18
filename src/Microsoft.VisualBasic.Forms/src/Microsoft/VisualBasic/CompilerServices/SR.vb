35adbfd3c088e6812dba406f1fda33dc42e019c8
Esme Povirk <esme@codeweavers.com>
2021-07-18 14:08:34 -0500

    Microsoft.VisualBasic.Forms: Add SR class

---
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/CompilerServices/SR.vb
@@ -0,0 +1,8 @@
+
+Namespace Microsoft.VisualBasic.CompilerServices
+	Friend Partial Class SR
+		Public Shared ReadOnly AppModel_SplashAndMainFormTheSame As String = "AppModel_SplashAndMainFormTheSame"
+		Public Shared ReadOnly General_ArgumentNullException As String = "General_ArgumentNullException"
+		Public Shared ReadOnly General_PropertyNothing As String = "General_PropertyNothing"
+	End Class
+End Namespace
